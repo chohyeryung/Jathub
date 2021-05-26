bbaf7bc5bdec8535497be47da6798adbf5defcd2
chohyeryung <chohyeryungcho@gmail.com>
2021-05-26 10:09:36 +0900

    jat/models Repository, Introduction, Comment

---
+++ jat/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+#ForeignKey를 introduction에 쓰는 이유는 introduction이 n이기 때문, repo는 1 보통 n인거에 foreignkey를 씀
+#makemigrations jat / migrate
+
+class Repository(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    #repo1.introduction_set.all -> 연결된 자소서들 다 가져옴
+
+    def __str__(self):
+        return self.name
+
+class Introduction(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)    #intro1.repository
+    version = models.IntegerField(default=1)
+    contents = models.TextField()
+    #intro1.comment_set.all -> 연결된 댓글들을 다 가져옴
+
+    def __str__(self):
+        return f'{self.version} {self.contents}'
+
+class Comment(models.Model):
+    introduction = models.ForeignKey(Introduction,on_delete=models.CASCADE)   #comm1.introduction
+    comment = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
