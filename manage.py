3b9923951f4b81aec5f46d6bd0cebaca69b8ace0
chohyeryung <chohyeryungcho@gmail.com>
2021-05-26 09:23:21 +0900

    startproject Jathub

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#사람 제외
+#사람 제외 local
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
