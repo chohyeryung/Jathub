8e67ca33e5bef08f73b41bc07d6cd619f90e819f
chohyeryung <chohyeryungcho@gmail.com>
2021-05-27 09:32:32 +0900

    jat/views RepositoryListView

--- jat/views.py
+++ jat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from jat.models import Repository
+
+
+class RepositoryListView(generic.ListView):
+    model = Repository
+
