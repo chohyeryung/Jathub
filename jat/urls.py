653254485f585a523b4d442c7bd2d3ce46faecb9
chohyeryung <chohyeryungcho@gmail.com>
2021-05-27 10:17:52 +0900

    jat/urls

--- jat/urls.py
+++ jat/urls.py
@@ -5,5 +5,6 @@ from jat import views
 app_name = 'jat'
 
 urlpatterns = [
-    path('', views.RepositoryListView.as_view(), name='repository_list')    #name은 맘대로 지정하는 것임
+    path('', views.RepositoryListView.as_view(), name='repository_list'),    #name은 맘대로 지정하는 것임
+    path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),
 ]
