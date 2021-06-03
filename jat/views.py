2d275d3f4dace745ef2322b460cea1691f97159a
chohyeryung <chohyeryungcho@gmail.com>
2021-06-03 09:48:52 +0900

    jat/views IntroductionCreateView, IntroductionDeleteView

--- jat/views.py
+++ jat/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db.models import Max
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -15,7 +16,7 @@ class RepositoryDetailView(generic.DetailView):
 
 class RepositoryCreateView(generic.CreateView):
     model = Repository
-    fields = ['name', 'description', 'deadline']    #'__all__' 전체를 읽어옴
+    fields = ['name', 'description', 'deadline']  # '__all__' 전체를 읽어옴
     template_name_suffix = '_create'
     success_url = reverse_lazy('jat:repository_list')
 
@@ -35,3 +36,35 @@ class RepositoryDeleteView(generic.DeleteView):
 class IntroductionDetailView(generic.DetailView):
     model = Introduction
 
+
+class IntroductionCreateView(generic.CreateView):
+    model = Introduction
+    fields = ['repository', 'version', 'contents', 'access']
+    template_name_suffix = '_create'
+
+    # success_url = reverse_lazy('jat:repository_detail') repository_detail은 pk가 필요함 -> 에러
+
+    def get_initial(self):
+        repository = get_object_or_404(Repository, pk=self.kwargs['repository_pk'])
+        introduction = repository.introduction_set.aggregate(Max('version'))    #해당 repository의 introduction version 최댓값 구하기
+        version = introduction['version__max']
+        if version is None:  # introduction이 아예 없으면 version 기본값은 1
+            version = 1
+        else:  # 있으면 최대값에서 +1
+            version += 1
+        return {'repository': repository, 'version': version}
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk': self.kwargs['repository_pk']})
+
+
+class IntroductionDeleteView(generic.DeleteView):
+    model = Introduction
+    success_url = reverse_lazy('jat:repository_detail')
+
+    def get_initial(self):
+        repository = get_object_or_404(Repository, pk=self.kwargs['repository_pk'])
+        return {'repository': repository}
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk': self.kwargs['repository_pk']})
